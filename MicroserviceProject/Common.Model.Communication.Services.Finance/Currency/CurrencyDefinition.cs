006700f6921786c7c307dee5fe1863c79b2251a7
Serkan Çamur <serkancamur@gmail.com>
2021-01-30 17:34:00 +0300

    Folder structure has been extended. HR and Finance models have been added.

---
+++ MicroserviceProject/Common.Model.Communication.Services.Finance/Currency/CurrencyDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Model.Communication.Services.Finance.Currency
+{
+    public class CurrencyDefinition
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ MicroserviceProject/Common.Model.Communication.Services.HR/People/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Model.Communication.Services.HR.People
+{
+    public class Person
+    {
+        public string IdentityNo { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string Surname { get; set; }
+    }
+}
