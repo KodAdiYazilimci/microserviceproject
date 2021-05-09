e059ed52675cbe4e2d98df1807d6237132b88be8
Serkan Çamur <serkancamur@gmail.com>
2021-05-09 22:11:50 +0300

    Alternative unit test method which calls services with communicator, has been implemented

--- MicroserviceProject/MicroserviceProject.Services.Business.Departments.HR.Test/Tests/DepartmentControllerUnitTest.cs
+++ MicroserviceProject/MicroserviceProject.Services.Business.Departments.HR.Test/Tests/DepartmentControllerUnitTest.cs
@@ -1,10 +1,18 @@
-﻿using MicroserviceProject.Services.Business.Departments.HR.Controllers;
+﻿using MicroserviceProject.Infrastructure.Communication.Model.Basics;
+using MicroserviceProject.Infrastructure.Communication.Moderator;
+using MicroserviceProject.Infrastructure.Routing.Providers;
+using MicroserviceProject.Services.Business.Departments.HR.Controllers;
 using MicroserviceProject.Services.Business.Departments.HR.Test.Factories.Services;
+using MicroserviceProject.Services.Business.Departments.HR.Test.Prepreations.Infrastructure;
+using MicroserviceProject.Services.Model.Department.HR;
+using MicroserviceProject.Test.Services.Factories;
+using MicroserviceProject.Test.Services.Providers.Configuration;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,12 +23,22 @@ namespace MicroserviceProject.Services.Business.Departments.HR.Test.Tests
     {
         private CancellationTokenSource cancellationTokenSource = null;
         private DepartmentController departmentController = null;
+        private RouteNameProvider routeNameProvider = null;
+        private ServiceCommunicator serviceCommunicator = null;
 
         [TestInitialize]
         public void Init()
         {
             cancellationTokenSource = new CancellationTokenSource();
             departmentController = new DepartmentController(DepartmentServiceFactory.Instance);
+            routeNameProvider = RouteNameProviderFactory.GetRouteNameProvider(ConfigurationFactory.GetConfiguration());
+
+            serviceCommunicator =
+                ServiceCommunicatorFactory.GetServiceCommunicator(
+                    memoryCache: MemoryCacheFactory.Instance,
+                    credentialProvider: CredentialProviderFactory.GetCredentialProvider(ConfigurationFactory.GetConfiguration()),
+                    routeNameProvider: routeNameProvider,
+                    serviceRouteRepository: ServiceRouteRepositoryFactory.GetServiceRouteRepository(ConfigurationFactory.GetConfiguration()));
         }
 
         [TestMethod]
@@ -30,6 +48,21 @@ namespace MicroserviceProject.Services.Business.Departments.HR.Test.Tests
 
             Assert.IsInstanceOfType(departmentResult, typeof(OkObjectResult));
         }
+
+        [TestMethod]
+        public async Task GetDepartmentsWithServiceCommunicatorTest()
+        {
+            ServiceResultModel<List<DepartmentModel>> serviceResult =
+                await
+                serviceCommunicator.Call<List<DepartmentModel>>(
+                serviceName: routeNameProvider.HR_GetDepartments,
+                postData: null,
+                queryParameters: null,
+                cancellationTokenSource: cancellationTokenSource);
+
+            Assert.IsTrue(serviceResult.IsSuccess);
+        }
+
 
         [TestMethod]
         public async Task CreateDepartmentTask()
@@ -46,8 +79,15 @@ namespace MicroserviceProject.Services.Business.Departments.HR.Test.Tests
         public void CleanUp()
         {
             cancellationTokenSource = null;
+
             departmentController.Dispose();
             departmentController = null;
+
+            serviceCommunicator.Dispose();
+            serviceCommunicator = null;
+
+            routeNameProvider.Dispose();
+            routeNameProvider = null;
         }
     }
 }
