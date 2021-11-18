7c3a7f1d409816f4b1600bf36d077613db9a6c09
Serkan Çamur <serkancamur@gmail.com>
2021-11-18 21:58:14 +0300

    Web UI project has been initialized

---
+++ MicroserviceProject/Presentation.UI.Web/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Presentation.UI.Web.Models;
+
+using System.Threading.Tasks;
+
+namespace Presentation.UI.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        //private readonly SignInManager<User> signInManager;
+
+        //public UserController(SignInManager<User> signInManager)
+        //{
+        //    this.signInManager = signInManager;
+        //}
+
+        [Route(nameof(Login))]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route(nameof(Login))]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login([FromForm] CredentialModel credential)
+        {
+            if (ModelState.IsValid)
+            {
+                //await signInManager.SignInAsync(new Infrastructure.Security.Model.User()
+                //{
+                //    Email = credential.Email,
+                //    Password = credential.Password
+                //}, isPersistent: true, authenticationMethod: AuthenticationMethods);
+
+                //await HttpContext.SignInAsync(
+                //    CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MicroserviceProject/Presentation.UI.Web/Models/CredentialModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.UI.Web.Models
+{
+    public class CredentialModel
+    {
+        [Display(Name = "E-posta:")]
+        [Required(ErrorMessage = "E-posta adresi gerekli")]
+        [EmailAddress(ErrorMessage = "Geçersiz e-posta adresi")]
+        public string Email { get; set; }
+
+        [Display(Name = "Parola:")]
+        [Required(ErrorMessage = "Parola gerekli")]
+        public string Password { get; set; }
+
+        [Display(Name = "Parola Tekrar:")]
+        [Required(ErrorMessage = "Parola tekrar etmeli")]
+        [Compare(nameof(Password), ErrorMessage = "Parola aynı olmalı")]
+        public string PasswordRepeat { get; set; }
+    }
+}
