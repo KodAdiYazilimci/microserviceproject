a4a2ca08001956988a4d6d130d3b60c298ae4f86
Serkan Çamur <serkancamur@gmail.com>
2023-02-18 19:44:51 +0300

    AccountControllerUnitTest test class has been complete

--- MicroserviceProject/Services.Api.Business.Departments.Accounting/Controllers/_BaseController.cs
+++ MicroserviceProject/Services.Api.Business.Departments.Accounting/Controllers/_BaseController.cs
@@ -1,11 +1,12 @@
-﻿using Infrastructure.Communication.Http.Wrapper;
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Services.Api.Business.Departments.Accounting.Controllers
 {
     public class BaseController : Controller
     {
-
+        /// <summary>
+        /// Request pipeline da MeditR kullanmayı atlar
+        /// </summary>
+        public bool ByPassMediatR { get; set; }
     }
 }
---
+++ MicroserviceProject/Test.Services.Api.Business.Departments/Accounting/AccountControllerTest.cs
@@ -0,0 +1,109 @@
+﻿using Infrastructure.Communication.Http.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Services.Api.Business.Departments.Accounting.Controllers;
+using Services.Communication.Http.Broker.Department.Accounting.CQRS.Commands.Requests;
+using Services.Communication.Http.Broker.Department.Accounting.Models;
+
+using Test.Services.Api.Business.Departments.Accounting.Factories.Services;
+
+namespace Test.Services.Api.Business.Departments.Accounting
+{
+    public class AccountControllerTest
+    {
+        private readonly AccountController accountController;
+
+        public AccountControllerTest()
+        {
+            accountController = new AccountController(null, BankServiceFactory.Instance);
+            accountController.ByPassMediatR = true;
+        }
+
+        public async Task<List<BankAccountModel>> GetBankAccountsOfWorkerAsync(int workerId)
+        {
+            IActionResult actionResult = await accountController.GetBankAccountsOfWorker(workerId);
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                var inventories = okObjectResult.Value as ServiceResultModel<List<BankAccountModel>>;
+
+                return inventories.Data;
+            }
+
+            return null;
+        }
+
+        public async Task<ServiceResultModel> CreateBankAccountTestAsync(CreateBankAccountCommandRequest createBankAccountCommandRequest)
+        {
+            IActionResult actionResult = await accountController.CreateBankAccount(createBankAccountCommandRequest);
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                return okObjectResult.Value as ServiceResultModel;
+            }
+
+            return null;
+        }
+
+        public async Task<List<CurrencyModel>> GetCurrenciesAsync()
+        {
+            IActionResult actionResult = await accountController.GetCurrencies();
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                return (okObjectResult.Value as ServiceResultModel<List<CurrencyModel>>).Data;
+            }
+
+            return null;
+        }
+
+        public async Task<ServiceResultModel> CreateCurrencyAsync(CreateCurrencyCommandRequest createCurrencyCommandRequest)
+        {
+            IActionResult actionResult = await accountController.CreateCurrency(createCurrencyCommandRequest);
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                return okObjectResult.Value as ServiceResultModel;
+            }
+
+            return null;
+        }
+
+        public async Task<List<SalaryPaymentModel>> GetSalaryPaymentsOfWorkerAsync(int workerId)
+        {
+            IActionResult actionResult = await accountController.GetSalaryPaymentsOfWorker(workerId);
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                return (okObjectResult.Value as ServiceResultModel<List<SalaryPaymentModel>>).Data;
+            }
+
+            return null;
+        }
+
+        public async Task<ServiceResultModel> CreateSalaryPaymentTest(CreateSalaryPaymentCommandRequest createSalaryPaymentCommandRequest)
+        {
+            IActionResult actionResult = await accountController.CreateSalaryPayment(createSalaryPaymentCommandRequest);
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                return okObjectResult.Value as ServiceResultModel;
+            }
+
+            return null;
+        }
+    }
+}
