d9b309c6003fce3d6a7c2415bb9b5bef69425cc2
Serkan Çamur <serkancamur@gmail.com>
2023-02-18 18:28:17 +0300

    The InventoryControllerUnitTest test class has been complete

---
+++ MicroserviceProject/Test.Services.Api.Business.Departments/HR/PersonControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using Infrastructure.Communication.Http.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Services.Api.Business.Departments.HR.Controllers;
+using Services.Communication.Http.Broker.Department.HR.CQRS.Commands.Requests;
+using Services.Communication.Http.Broker.Department.HR.Models;
+
+using Test.Services.Api.Business.Departments.HR.Factories.Services;
+
+namespace Test.Services.Api.Business.Departments.HR
+{
+    public class PersonControllerTest
+    {
+        private readonly PersonController personController;
+
+        public PersonControllerTest()
+        {
+            personController = new PersonController(null, PersonServiceFactory.Instance);
+            personController.ByPassMediatR = true;
+        }
+
+        public async Task<List<WorkerModel>> GetWorkersAsync()
+        {
+            IActionResult actionResult = await personController.GetWorkers();
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                var workers = okObjectResult.Value as ServiceResultModel<List<WorkerModel>>;
+
+                return workers.Data;
+            }
+
+            return null;
+        }
+
+        public async Task<ServiceResultModel> CreateWorkerAsync(CreateWorkerCommandRequest createWorkerCommandRequest)
+        {
+            IActionResult actionResult = await personController.CreateWorker(createWorkerCommandRequest);
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                return okObjectResult.Value as ServiceResultModel;
+            }
+
+            return null;
+        }
+
+        public async Task<ServiceResultModel> CreatePersonAsync(CreatePersonCommandRequest createPersonCommandRequest)
+        {
+            IActionResult actionResult = await personController.CreatePerson(createPersonCommandRequest);
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                return okObjectResult.Value as ServiceResultModel;
+            }
+
+            return null;
+        }
+
+        public async Task<List<PersonModel>> GetPeopleAsync()
+        {
+            IActionResult actionResult = await personController.GetPeople();
+
+            if (actionResult is OkObjectResult)
+            {
+                OkObjectResult okObjectResult = (OkObjectResult)actionResult;
+
+                return (okObjectResult.Value as ServiceResultModel<List<PersonModel>>).Data;
+            }
+
+            return null;
+        }
+    }
+}
