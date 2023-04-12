002ceee325ada1e4e0dc53e846f78da91cea6416
Serkan Çamur <serkancamur@gmail.com>
2023-04-12 18:53:22 +0300

    Dependency injections classed have been generated for Service Discoverers

---
+++ MicroserviceProject/Infrastructure.ServiceDiscovery.Discoverer/_DI/ServiceDiscovererConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Caching.InMemory.DI;
+using Infrastructure.Communication.Http.Broker.DI;
+using Infrastructure.ServiceDiscovery.Abstract;
+using Infrastructure.ServiceDiscovery.Discoverer.Abstract;
+using Infrastructure.ServiceDiscovery.Discoverer.Configuration;
+using Infrastructure.ServiceDiscovery.Discoverer.Discovers;
+using Infrastructure.ServiceDiscovery.Providers;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.ServiceDiscovery.Discoverer._DI
+{
+    public static class ServiceDiscovererConfiguration
+    {
+        public static IServiceCollection RegisterServiceDiscoverers(this IServiceCollection services)
+        {
+            services.RegisterInMemoryCaching();
+            services.RegisterHttpServiceCommunicator();
+
+            services.AddSingleton<ISolidServiceConfiguration, AppConfigSolidServiceConfiguration>();
+            services.AddSingleton<ISolidServiceProvider, AppConfigSolidServiceProvider>();
+            services.AddSingleton<IServiceDiscoverer, HttpServiceDiscoverer>();
+
+            return services;
+        }
+    }
+}
---
+++ MicroserviceProject/Infrastructure.ServiceDiscovery.Register/_DI/ServiceRegistererConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Communication.Http.Broker.DI;
+using Infrastructure.ServiceDiscovery.Abstract;
+using Infrastructure.ServiceDiscovery.Providers;
+using Infrastructure.ServiceDiscovery.Register.Abstract;
+using Infrastructure.ServiceDiscovery.Register.Registerers;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.ServiceDiscovery.Register._DI
+{
+    public static class ServiceRegistererConfiguration
+    {
+        public static IServiceCollection RegisterServiceRegisterers(this IServiceCollection services)
+        {
+            services.RegisterHttpServiceCommunicator();
+
+            services.AddSingleton<ISolidServiceProvider, AppConfigSolidServiceProvider>();
+            services.AddSingleton<IServiceRegisterer, HttpServiceRegisterer>();
+
+            return services;
+        }
+    }
+}
